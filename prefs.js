2d899a5357bd9ed106a092abcd2847c048753610
raujonas <rau.jonas@web.de>
2020-08-31 16:14:19 +0200

    Refactor view as three tabs for each location

--- prefs.js
+++ prefs.js
@@ -21,36 +21,52 @@ function buildPrefsWidget() {
         settings_schema: gschema.lookup('org.gnome.shell.extensions.executor', true)
     });
 
+    let notebook = new Gtk.Notebook({
+        visible: true
+    });
+    
     let prefsWidget = new Gtk.Grid({
-        margin: 18,
+        /*margin: 18,
         column_spacing: 12,
-        row_spacing: 12,
+        row_spacing: 12,*/
         visible: true,
         column_homogeneous: true,
     });
 
+    prefsWidget.attach(notebook, 0, 0, 1, 1);
+
     /* LEFT */
+    let leftGrid = new Gtk.Grid({
+        /*margin: 18,*/
+        column_spacing: 12,
+        row_spacing: 12,
+        visible: true,
+        column_homogeneous: true,
+        vexpand: true,
+        hexpand: true
+    });
+
     let leftLabel = new Gtk.Label({
-        label: '<b>Left:</b>',
+        label: '<b>Active:</b>',
         halign: Gtk.Align.START,
         use_markup: true,
         visible: true
     });
-    prefsWidget.attach(leftLabel, 0, 0, 1, 1);
+    leftGrid.attach(leftLabel, 0, 0, 1, 1);
 
     let leftActive = new Gtk.Switch({
     	valign: Gtk.Align.END,
     	halign: Gtk.Align.END,
     	visible: true
     });
-    prefsWidget.attach(leftActive, 1, 0, 1, 1);
+    leftGrid.attach(leftActive, 1, 0, 1, 1);
 
     let leftIndexLabel = new Gtk.Label({
         label: 'Index:',
         halign: Gtk.Align.START,
         visible: true
     });
-    prefsWidget.attach(leftIndexLabel, 0, 1, 1, 1);
+    leftGrid.attach(leftIndexLabel, 0, 1, 1, 1);
 
     let leftIndex = new Gtk.SpinButton({
         adjustment: new Gtk.Adjustment({
@@ -60,42 +76,59 @@ function buildPrefsWidget() {
         }),
         visible: true
     });
-    prefsWidget.attach(leftIndex, 1, 1, 1, 1);
+    leftGrid.attach(leftIndex, 1, 1, 1, 1);
 
     let leftCommandsJsonLabel = new Gtk.Label({
         label: 'Commands as JSON:',
         halign: Gtk.Align.START,
         visible: true
     });
-    prefsWidget.attach(leftCommandsJsonLabel, 0, 2, 1, 1);
+    leftGrid.attach(leftCommandsJsonLabel, 0, 2, 1, 1);
 
     let leftCommandsJson = new Gtk.Entry({
         visible: true
     });
-    prefsWidget.attach(leftCommandsJson, 1, 2, 1, 1);
+    leftGrid.attach(leftCommandsJson, 1, 2, 1, 1);
+
+    let pageLeft = new Gtk.Box({
+        visible: true
+    });
+    pageLeft.border_width = 10;
+    pageLeft.add(leftGrid);
+    notebook.append_page(pageLeft,new Gtk.Label({label: "Left", visible: true}));
 
     /* CENTER */
+    let centerGrid = new Gtk.Grid({
+        /*margin: 18,*/
+        column_spacing: 12,
+        row_spacing: 12,
+        visible: true,
+        column_homogeneous: true,
+        vexpand: true,
+        hexpand: true
+    });
+
     let centerLabel = new Gtk.Label({
-        label: '<b>Center:</b>',
+        label: '<b>Active:</b>',
         halign: Gtk.Align.START,
         use_markup: true,
         visible: true
     });
-    prefsWidget.attach(centerLabel, 0, 3, 1, 1);
+    centerGrid.attach(centerLabel, 0, 3, 1, 1);
 
     let centerActive = new Gtk.Switch({
     	valign: Gtk.Align.END,
     	halign: Gtk.Align.END,
     	visible: true
     });
-    prefsWidget.attach(centerActive, 1, 3, 1, 1);
+    centerGrid.attach(centerActive, 1, 3, 1, 1);
 
     let centerIndexLabel = new Gtk.Label({
         label: 'Index:',
         halign: Gtk.Align.START,
         visible: true
     });
-    prefsWidget.attach(centerIndexLabel, 0, 4, 1, 1);
+    centerGrid.attach(centerIndexLabel, 0, 4, 1, 1);
 
     let centerIndex = new Gtk.SpinButton({
         adjustment: new Gtk.Adjustment({
@@ -105,42 +138,59 @@ function buildPrefsWidget() {
         }),
         visible: true
     });
-    prefsWidget.attach(centerIndex, 1, 4, 1, 1);
+    centerGrid.attach(centerIndex, 1, 4, 1, 1);
 
     let centerCommandsJsonLabel = new Gtk.Label({
         label: 'Commands as JSON:',
         halign: Gtk.Align.START,
         visible: true
     });
-    prefsWidget.attach(centerCommandsJsonLabel, 0, 5, 1, 1);
+    centerGrid.attach(centerCommandsJsonLabel, 0, 5, 1, 1);
 
     let centerCommandsJson = new Gtk.Entry({
         visible: true
     });
-    prefsWidget.attach(centerCommandsJson, 1, 5, 1, 1);
+    centerGrid.attach(centerCommandsJson, 1, 5, 1, 1);
+
+    let pageCenter = new Gtk.Box({
+        visible: true
+    });
+    pageCenter.border_width = 10;
+    pageCenter.add(centerGrid)
+    notebook.append_page(pageCenter,new Gtk.Label({label: "Center", visible: true}));
 
     /* RIGHT */
+    let rightGrid = new Gtk.Grid({
+        /*margin: 18,*/
+        column_spacing: 12,
+        row_spacing: 12,
+        visible: true,
+        column_homogeneous: true,
+        vexpand: true,
+        hexpand: true
+    });
+
     let rightLabel = new Gtk.Label({
-        label: '<b>Right:</b>',
+        label: '<b>Active:</b>',
         halign: Gtk.Align.START,
         use_markup: true,
         visible: true
     });
-    prefsWidget.attach(rightLabel, 0, 6, 1, 1);
+    rightGrid.attach(rightLabel, 0, 6, 1, 1);
 
     let rightActive = new Gtk.Switch({
     	valign: Gtk.Align.END,
     	halign: Gtk.Align.END,
     	visible: true
     });
-    prefsWidget.attach(rightActive, 1, 6, 1, 1);
+    rightGrid.attach(rightActive, 1, 6, 1, 1);
 
     let rightIndexLabel = new Gtk.Label({
         label: 'Index:',
         halign: Gtk.Align.START,
         visible: true
     });
-    prefsWidget.attach(rightIndexLabel, 0, 7, 1, 1);
+    rightGrid.attach(rightIndexLabel, 0, 7, 1, 1);
 
     let rightIndex = new Gtk.SpinButton({
         adjustment: new Gtk.Adjustment({
@@ -150,19 +200,26 @@ function buildPrefsWidget() {
         }),
         visible: true
     });
-    prefsWidget.attach(rightIndex, 1, 7, 1, 1);
+    rightGrid.attach(rightIndex, 1, 7, 1, 1);
 
     let commandsJsonLabel = new Gtk.Label({
         label: 'Commands as JSON:',
         halign: Gtk.Align.START,
         visible: true
     });
-    prefsWidget.attach(commandsJsonLabel, 0, 8, 1, 1);
+    rightGrid.attach(commandsJsonLabel, 0, 8, 1, 1);
 
     let rightCommandsJson = new Gtk.Entry({
         visible: true
     });
-    prefsWidget.attach(rightCommandsJson, 1, 8, 1, 1);
+    rightGrid.attach(rightCommandsJson, 1, 8, 1, 1);
+
+    let pageRight = new Gtk.Box({
+        visible: true
+    });
+    pageRight.border_width = 10;
+    pageRight.add(rightGrid)
+    notebook.append_page(pageRight,new Gtk.Label({label: "Right", visible: true}));
 
     settings.bind('left-active', leftActive, 'active', Gio.SettingsBindFlags.DEFAULT);
     settings.bind('left-index', leftIndex, 'value', Gio.SettingsBindFlags.DEFAULT);
