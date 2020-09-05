0503e9219cd0c9f8c8c5651cae773847e2e25159
Jonas Rau <rau.jonas@web.de>
2020-09-05 18:38:51 +0200

    Update make-release.bash

--- make-release.bash
+++ make-release.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "make release and set version to $1"
-./set-version.bash 3
+./set-version.bash $1
 zip -r executor@raujonas.github.io schemas/ extension.js metadata.json prefs.js 
 echo "done"
