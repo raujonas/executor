617a0a1029f86ba24aed7d24b20875f35c146207
raujonas <rau.jonas@web.de>
2020-09-05 21:12:55 +0200

    Add error handling in release scripts

--- make-release.bash
+++ make-release.bash
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-echo "compile settings schema"
-glib-compile-schemas ./schemas
-echo "set version:"
-./set-version.bash $1
-echo "create zip"
-zip -r executor@raujonas.github.io schemas/ extension.js metadata.json prefs.js 
-echo "done"
+if [ $# -eq 0 ]
+  then
+    echo "No version argument given"
+  else
+    echo "compile settings schema"
+    glib-compile-schemas ./schemas
+    echo "set version:"
+    ./set-version.bash $1
+    echo "create zip"
+    zip -r executor@raujonas.github.io schemas/ extension.js metadata.json prefs.js 
+    echo "done"
+fi
+
--- set-version.bash
+++ set-version.bash
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-echo "set version to $1"
-mv metadata.json temp.json
-jq -r --argjson V $1 '.version |= $V' temp.json > metadata.json
-rm temp.json
-echo "done"
+if [ $# -eq 0 ]
+  then
+    echo "No version argument given"
+  else
+    echo "set version to $1"
+    mv metadata.json temp.json
+    jq -r --argjson V $1 '.version |= $V' temp.json > metadata.json
+    rm temp.json
+    echo "done"
+fi
