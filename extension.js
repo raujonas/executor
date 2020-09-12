5fc86d5a8b7e801041063b387d1b7c1a5cb534dc
raujonas <rau.jonas@web.de>
2020-09-12 08:51:59 +0200

    Update log when no commands specified

--- extension.js
+++ extension.js
@@ -286,7 +286,7 @@ function checkCommands(location, json) {
         this.setOutput(location);
 
     } else {
-        log('No commands specified');
+        log('No commands specified: ' + location.name);
         location.commandsOutput = [];
         this.setOutput(location);
     }
