b56a5e4bc26e0b6324ef94e26fab2e13afec08aa
raujonas <rau.jonas@web.de>
2021-05-10 10:44:55 +0200

    Bump version

--- make-release.bash
+++ make-release.bash
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
-
-if [ $# -eq 0 ]
-  then
-    echo "No version argument given"
-  else
-    echo "compile settings schema"
-    glib-compile-schemas ./schemas
-    echo "set version:"
-    ./set-version.bash $1
-    echo "create zip"
-    zip -r executor@raujonas.github.io schemas/ extension.js metadata.json prefs.js custom.css stylesheet.css
-    echo "done"
-fi
-
+echo "compile settings schema"
+glib-compile-schemas ./schemas
+echo "create zip"
+zip -r executor@raujonas.github.io schemas/ extension.js metadata.json prefs.js custom.css stylesheet.css
+echo "done"
